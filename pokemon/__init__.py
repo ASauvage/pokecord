4ff14e2dbc15e8c330c7683139524e134a6d1c6d
Alexandre Sauvage Potel <a.sauvagepotel@gmail.com>
2024-12-18 00:00:43 +0100

    [feature] class pokemon

--- pokemon/__init__.py
+++ pokemon/__init__.py
@@ -1,11 +1,33 @@
 from .pokeapi import PokeAPI
 
 
-class Pokemon:
+class PokeData:
     def __init__(self, id: int):
-        self._id = id
-        self.pokemon = PokeAPI.get_pokemon_by_id(id)
+        self.pokeapi_data = PokeAPI().get_pokemon_by_id(id)
+
+    @property
+    def number(self) -> int:
+        return self.pokeapi_data['number']
 
     @property
     def name(self) -> str:
-        return self.pokemon['name']
+        return self.pokeapi_data['name']
+
+
+class Pokemon(PokeData):
+    def __init__(self, id: int):
+        self._id = id
+        self.pokemon = dict()
+        super().__init__(self.pokemon['number'])
+
+    @property
+    def nickname(self) -> str | None:
+        return self.pokemon['nickname']
+
+    @property
+    def sexe(self) -> bool:
+        return self.pokemon['sexe']
+
+    @property
+    def level(self) -> int:
+        return self.pokemon['level']

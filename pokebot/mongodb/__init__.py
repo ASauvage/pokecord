6311e0f05e780b6b91d62fc9c85a9f7b5a44b9bb
Alexandre <a.sauvagepotel@gmail.com>
2024-12-19 20:10:37 +0100

    [feature] import subfunctions

--- pokebot/mongodb/__init__.py
+++ pokebot/mongodb/__init__.py
@@ -4,3 +4,7 @@ import pymongo
 class MongoCon(pymongo.MongoClient):
     def __init__(self, host: str = "mongodb://localhost:27017/"):
         super().__init__(host)
+
+    from ._trainer import get_trainer_info, is_trainer_exist, create_trainer
+    from ._pokemon import add_pokemon
+    # from ._inventory import
